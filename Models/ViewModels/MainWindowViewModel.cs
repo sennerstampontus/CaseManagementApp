298ed555cbde2a5b28f1710c881f7beadc542067
sennerstampontus <sennerstam.pontus@gmail.com>
2022-01-11 10:50:59 +0100

    First Commit

---
+++ Models/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CaseManagementApp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseManagementApp.Models.ViewModels
+{
+    internal class MainWindowViewModel : ObservableObject
+    {
+        public RelayCommand CreateCustomerCommand { get; set; }
+        public RelayCommand CreateAdminCommand { get; set; }
+    }
+}
